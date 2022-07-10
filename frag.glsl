2daf6c4b5cfad1b2b7f5a81aa559f4c6d9b495a1
yugecin <yugecin@users.noreply.github.com>
2022-07-10 18:43:09 +0200

    bad lighting
    because it's too dark where the wall will be

--- frag.glsl
+++ frag.glsl
@@ -8,7 +8,7 @@
 layout (location=0) uniform vec4 fpar[2];
 layout (location=2) uniform vec4 debug[2];
 out vec4 c;
-in vec2 p;
+in vec2 v;
 float rand(vec2 p){return fract(sin(dot(p.xy,vec2(12.9898,78.233)))*43758.5453);}
 
 mat2 rot2(float a){float s=sin(a),c=cos(a);return mat2(c,s,-s,c);}
@@ -170,16 +170,71 @@ vec3 GetRayDir(vec2 uv, vec3 p, vec3 l, float zoom) {
 	return d;
 }
 
+float flopine_shade;
+vec3 p;
+vec3 march(vec3 o,vec3 v){ // x=hit y=dist_to_p z=tot_dist
+	vec3 r=vec3(0);
+	for(int i=0;i<200;i++){
+		p=o+r.z*v;
+
+		//p.y += 100.;
+		//p.z -= 10.;
+		//p.yz *= rot2(-.9);
+		//p = mod(p, 30.) - 15.;
+		op = p;
+		//p.xy *= rot2(iTime/2.);
+		//p.yz *= rot2(iTime/3.);
+		//p.xz*=rot2(sin(p.z*0.2)*0.2+iTime);
+		float d=map(p);
+		if (d < .008) {
+			//vec3 n = norm(p, d);
+			//vec3 r = reflect(rd, n);
+
+			//col = n; // norm colors
+			r.y=d;
+			flopine_shade=1.-float(i)/100.;
+			//float dif = dot(n, normalize(vec3(1,2,-3)))*.5+.5;
+			//col = vec3(dif);
+			//col = vec3(1.,.745,.07);
+			//col *= 1.-float(i)/90.;
+
+/*
+			col = vec3(.1);
+			//col += .02 * dot(n,normalize(-rd));
+			vec3 lightsrc = vec3(16.,10.,-10.);
+			col += vec3(1.,.92,.71) * .2 * dot(n,normalize(lightsrc-p)) / pow(length(p-lightsrc) / 125., 2);
+			lightsrc = vec3(-16.,10.,-10.);
+			col += vec3(1.,.92,.71) * .2 * dot(n,normalize(lightsrc-p)) / pow(length(p-lightsrc) / 15., 2.);
+			//col = vec3(.05 + .05 * dot(n,normalize(-rd)));
+			*/
+
+			r.x=1;
+			break;
+		}
+		r.z+=d;
+	}
+	return r;
+}
+
+float lit(vec3 h,vec2 a, vec3 n) {
+	vec3 b=vec3(a,-55),
+		r=march(b,normalize(h-b));
+	if (r.x>0 && length(p-h)<.1) {
+		return .2 * dot(n,normalize(b-h)) / pow(r.z / 65., 4.);
+	}
+	return 0.;
+}
+
 void main()
 {
-	vec2 uv = vec2(p.x,p.y/1.77);
+	vec2 uv=v;uv.y/=1.77;
 	/*
 	if (mod(gl_FragCoord.x, 2.) < 1.) {
 		uv.y += .1;
 	}
 	*/
 
-	float ttt = rand(p)*.01 + iTime;
+	float ttt = rand(v)*.001 + iTime;
 	vec3 ro = vec3(10*sin(ttt), -30*cos(ttt), -20);
 
 	//vec3 ro = vec3(20, -50, -20);
@@ -190,7 +245,7 @@ void main()
 	vec3 rd = GetRayDir(uv, ro, vec3(0,0,0), 1.);
 
 
-	vec3 at = vec3(0);
+	vec3 at = vec3(0,0,-25);
 
 #if debugmov
 	ro = debug[0].xyz;
@@ -213,43 +268,57 @@ void main()
 	rd = rayDirection;
 
 	vec3 col = vec3(.1);//vec3(.1-length(uv)*.1);
-	float tot_dist = 0.;
-	bool hit = false;
-	for(int i=0; i < 200 /*&& tot_dist < 400.*/; i++) {
-		vec3 p = ro + tot_dist * rd;
-
-		//p.y += 100.;
-		//p.z -= 10.;
-		//p.yz *= rot2(-.9);
-		//p = mod(p, 30.) - 15.;
-		op = p;
-		//p.xy *= rot2(iTime/2.);
-		//p.yz *= rot2(iTime/3.);
-		//p.xz*=rot2(sin(p.z*0.2)*0.2+iTime);
-		float d = map(p);
-		if (d < .001) {
-			vec3 n = norm(p, d);
-			//vec3 r = reflect(rd, n);
-
-			//col = n; // norm colors
-			col = vec3(1.-float(i)/100.); // flopine shade
-			//float dif = dot(n, normalize(vec3(1,2,-3)))*.5+.5;
-			//col = vec3(dif);
-			//col = vec3(1.,.745,.07);
-			//col *= 1.-float(i)/90.;
-
+	vec3 r = march(ro,rd);
+	if (r.x>0) {
+		// hit
+		col=vec3(flopine_shade);
+		if (p.y>-99) {
 			col = vec3(.1);
-			//col += .02 * dot(n,normalize(-rd));
-			vec3 lightsrc = vec3(16.,10.,-10.);
-			col += vec3(1.,.92,.71) * .2 * dot(n,normalize(lightsrc-p)) / pow(length(p-lightsrc) / 125., 2);
-			lightsrc = vec3(-16.,10.,-10.);
-			col += vec3(1.,.92,.71) * .2 * dot(n,normalize(lightsrc-p)) / pow(length(p-lightsrc) / 15., 2.);
-			//col = vec3(.05 + .05 * dot(n,normalize(-rd)));
-
-			hit = true;
-			break;
+			col+=vec3(.05*rand(p.xy));
+			//col=vec3(flopine_shade);
+			vec3 n = norm(p, r.y);
+			vec2 lo=p.xy,of=vec2(80,100),mu=vec2(-1,1);
+			lo.y-=mod(lo.y+50,200)-100;
+			lo.x-=mod(lo.x-160,320)-160;
+			vec3 xx=p;
+			//col += vec3(1.,.92,.71) * lit(xx,lo+0*of*mu.xy,n);
+			col += vec3(1.,.92,.71) * lit(xx,lo/*+of*mu.yx*/,n);
+			//col += vec3(1.,.92,.71) * lit(xx,lo+vec2(0,200),n);
+			//col += vec3(1.,.92,.71) * lit(xx,lo+vec2(0,-200),n);
+			//col += vec3(1.,.92,.71) * lit(xx,lo+of*mu.yy,n);
+			//col += vec3(1.,.92,.71) * lit(xx,lo+of*mu.xy,n);
+			//col += vec3(1.,.92,.71) * lit(xx,lo+of*mu.xx,n);
+			//l=vec3(75,100,-55);
+			/*
+			vec3 xx=p;
+			r = march(l,normalize(xx-l));
+			if (r.x>0 && length(p-xx)<.1) {
+				//col = vec3(r.z/100);
+				col += vec3(1.,.92,.71) * .2 * dot(n,normalize(l-xx)) / pow(r.z / 75., 1.4);
+			}
+			/*
+			vec3 xx=p;
+			l=vec3(75,100,-55);
+			r = march(l,normalize(xx-l));
+			if (r.x>0 && length(p-xx)<.1) {
+				//col = vec3(r.z/100);
+				col += vec3(1.,.92,.71) * .2 * dot(n,normalize(l-xx)) / pow(r.z / 75., 1.4);
+			}
+			l=vec3(-75,100,-55);
+			r = march(l,normalize(xx-l));
+			if (r.x>0 && length(p-xx)<.1) {
+				//col = vec3(r.z/100);
+				col += vec3(1.,.92,.71) * .2 * dot(n,normalize(l-xx)) / pow(r.z / 75., 1.4);
+			}
+			/*
+			l=vec3(-80,50,-48);
+			r = march(l,normalize(xx-l));
+			if (r.x>0 && length(p-xx)<.1) {
+				//col = vec3(r.z/100);
+				col += vec3(1.,.92,.71) * .2 * dot(n,normalize(l-xx)) / pow(r.z / 95., 1.4);
+			}
+			*/
 		}
-		tot_dist += d;
 	}
 
 	//if (!hit) {
